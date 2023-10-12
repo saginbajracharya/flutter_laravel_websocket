00ead0169f4011b2e2e0a0f5fbd76dc5d45e9e68
sagin bajracharya <saginbajracharya09@gmail.com>
2023-10-12 15:25:55 +0545

    [fixed]::connect , disconnect,subscribe,unsubscribe to channel with package laravel_flutter_pusher: ^0.0.4

--- lib/src/sample_feature/sample_item_list_view.dart
+++ lib/src/sample_feature/sample_item_list_view.dart
@@ -1,10 +1,10 @@
-import 'package:flutter/foundation.dart';
+import 'dart:developer';
 import 'package:flutter/material.dart';
+// ignore: depend_on_referenced_packages
+import 'package:laravel_flutter_pusher/laravel_flutter_pusher.dart';
 
 import '../settings/settings_view.dart';
 import 'sample_item.dart';
-// ignore: depend_on_referenced_packages
-import 'package:socket_io_client/socket_io_client.dart' as socket_io;
 
 class SampleItemListView extends StatefulWidget {
   const SampleItemListView({
@@ -21,87 +21,28 @@ class SampleItemListView extends StatefulWidget {
 }
 
 class _SampleItemListViewState extends State<SampleItemListView> {
-  static const List events = [
-    'connect',
-    'connect_error',
-    'connect_timeout',
-    'connecting',
-    'disconnect',
-    'error',
-    'reconnect',
-    'reconnect_attempt',
-    'reconnect_failed',
-    'reconnect_error',
-    'reconnecting',
-    'ping',
-    'pong'
-  ];
-
-  late socket_io.Socket socket;
-  final serverUrl = 'http://192.168.1.106:6001';
-
+  var options = PusherOptions(
+    host: '192.168.1.106',
+    port: 6001,
+    encrypted: false,
+    cluster: 'ap2'
+  );
+  late LaravelFlutterPusher pusher;
   bool isConnected = false;
 
   @override
   void initState() {
     super.initState();
-    socket = socket_io.io(serverUrl, <String, dynamic>{
-      'transports': ['websocket'],
-      'autoConnect': true,
-    });
-
-    // Connect to the server
-    socket.connect();
-
-    socket.on('connect', (_) {
-      setState(() {
-        isConnected = true;
-      });
-    });
-
-    socket.on('disconnect', (_) {
-      setState(() {
-        isConnected = false;
-      });
-    });
-
-    socket.on('error', (error) {
-      if (kDebugMode) {
-        print('Error during connection: $error');
-      }
-    });
-
-    socket.on('home', (data) {
-      if (kDebugMode) {
-        print('Received message: $data');
-      }
-    });
-
-    socket.on('event', (data) {
-      if (kDebugMode) {
-        print(data);
-      }
-    });
-
-    socket.on('fromServer', (_) {
-      if (kDebugMode) {
-        print(_);
-      }
-    });
+    pusher = LaravelFlutterPusher("0d839545946ce754eb6c", options, enableLogging: true);
   }
 
   @override
   void dispose() {
-    // Disconnect the socket when disposing of the widget.
-    socket.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    if (kDebugMode) {
-      print(events);
-    }
     return Scaffold(
       appBar: AppBar(
         title: const Text('Socket Test'),
@@ -117,11 +58,12 @@ class _SampleItemListViewState extends State<SampleItemListView> {
       body: SizedBox(
         width: MediaQuery.of(context).size.width,
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisSize: MainAxisSize.max,
           children: [
             const SizedBox(height: 10),
+            //Connection Status
             Text(
               isConnected ? 'Connected' : 'Disconnected',
               style: TextStyle(
@@ -130,25 +72,53 @@ class _SampleItemListViewState extends State<SampleItemListView> {
               ),
             ),
             const SizedBox(height: 10),
+            //Connect Button
             TextButton(
               onPressed: () async{
-                socket.connect();
+                pusher.connect().then((value) {
+                  setState(() {
+                    isConnected=true;
+                  });
+                });
               },
               child: const Text('CONNECT'),
             ),
             const SizedBox(height: 10),
+            //Disconnect Button
             TextButton(
               onPressed: () async{
-                socket.emit('home', 'Hello from Flutter');
-              },
-              child: const Text('Send Message'),
-            ),
-            const SizedBox(height: 10),
-            TextButton(
-              onPressed: () async{
-                socket.disconnect();
+                pusher.disconnect().then((value) {
+                  setState(() {
+                    isConnected=false;
+                  });
+                });
               },
               child: const Text('DISCONNECT'),
+            ),
+            const SizedBox(height: 10),
+            //Suscribe Button
+            TextButton(
+              onPressed: () async{
+                pusher.subscribe('home').bind('event', (event) {
+                  log('event =>$event');
+                });
+              },
+              child: const Text('Suscribe Home CHANNEL'),
+            ),
+            //UnSubscribe Button
+            TextButton(
+              onPressed: () async{
+                pusher.unsubscribe('home');
+              },
+              child: const Text('Unsubscribe Home CHANNEL'),
+            ),
+            const SizedBox(height: 10),
+            //Send Message Button
+            TextButton(
+              onPressed: () async{
+                // pusher.trigger('my-event', {'message': 'Hello from Flutter!'});
+              },
+              child: const Text('SendMessage TO CHANNEL HOME'),
             ),
           ],
         ),
