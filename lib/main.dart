2686e9077f886adde91b5408da9997fc935f097a
sagin bajracharya <saginbajracharya09@gmail.com>
2023-10-12 11:25:20 +0545

    [managed]::for socket _io package not ok

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'src/app.dart';
 import 'src/settings/settings_controller.dart';
 import 'src/settings/settings_service.dart';
@@ -8,7 +7,6 @@ void main() async {
   // Set up the SettingsController, which will glue user settings to multiple
   // Flutter Widgets.
   final settingsController = SettingsController(SettingsService());
-
   // Load the user's preferred theme while the splash screen is displayed.
   // This prevents a sudden theme change when the app is first displayed.
   await settingsController.loadSettings();
--- lib/src/sample_feature/sample_item_list_view.dart
+++ lib/src/sample_feature/sample_item_list_view.dart
@@ -1,11 +1,12 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import '../settings/settings_view.dart';
 import 'sample_item.dart';
-import 'sample_item_details_view.dart';
+// ignore: depend_on_referenced_packages
+import 'package:socket_io_client/socket_io_client.dart' as socket_io;
 
-/// Displays a list of SampleItems.
-class SampleItemListView extends StatelessWidget {
+class SampleItemListView extends StatefulWidget {
   const SampleItemListView({
     super.key,
     this.items = const [SampleItem(1), SampleItem(2), SampleItem(3)],
@@ -16,55 +17,141 @@ class SampleItemListView extends StatelessWidget {
   final List<SampleItem> items;
 
   @override
+  State<SampleItemListView> createState() => _SampleItemListViewState();
+}
+
+class _SampleItemListViewState extends State<SampleItemListView> {
+  static const List events = [
+    'connect',
+    'connect_error',
+    'connect_timeout',
+    'connecting',
+    'disconnect',
+    'error',
+    'reconnect',
+    'reconnect_attempt',
+    'reconnect_failed',
+    'reconnect_error',
+    'reconnecting',
+    'ping',
+    'pong'
+  ];
+
+  late socket_io.Socket socket;
+  final serverUrl = 'http://192.168.1.106:6001';
+
+  bool isConnected = false;
+
+  @override
+  void initState() {
+    super.initState();
+    socket = socket_io.io(serverUrl, <String, dynamic>{
+      'transports': ['websocket'],
+      'autoConnect': true,
+    });
+
+    // Connect to the server
+    socket.connect();
+
+    socket.on('connect', (_) {
+      setState(() {
+        isConnected = true;
+      });
+    });
+
+    socket.on('disconnect', (_) {
+      setState(() {
+        isConnected = false;
+      });
+    });
+
+    socket.on('error', (error) {
+      if (kDebugMode) {
+        print('Error during connection: $error');
+      }
+    });
+
+    socket.on('home', (data) {
+      if (kDebugMode) {
+        print('Received message: $data');
+      }
+    });
+
+    socket.on('event', (data) {
+      if (kDebugMode) {
+        print(data);
+      }
+    });
+
+    socket.on('fromServer', (_) {
+      if (kDebugMode) {
+        print(_);
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    // Disconnect the socket when disposing of the widget.
+    socket.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    if (kDebugMode) {
+      print(events);
+    }
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Sample Items'),
+        title: const Text('Socket Test'),
         actions: [
           IconButton(
             icon: const Icon(Icons.settings),
             onPressed: () {
-              // Navigate to the settings page. If the user leaves and returns
-              // to the app after it has been killed while running in the
-              // background, the navigation stack is restored.
               Navigator.restorablePushNamed(context, SettingsView.routeName);
             },
           ),
         ],
       ),
-
-      // To work with lists that may contain a large number of items, it’s best
-      // to use the ListView.builder constructor.
-      //
-      // In contrast to the default ListView constructor, which requires
-      // building all Widgets up front, the ListView.builder constructor lazily
-      // builds Widgets as they’re scrolled into view.
-      body: ListView.builder(
-        // Providing a restorationId allows the ListView to restore the
-        // scroll position when a user leaves and returns to the app after it
-        // has been killed while running in the background.
-        restorationId: 'sampleItemListView',
-        itemCount: items.length,
-        itemBuilder: (BuildContext context, int index) {
-          final item = items[index];
-
-          return ListTile(
-            title: Text('SampleItem ${item.id}'),
-            leading: const CircleAvatar(
-              // Display the Flutter Logo image asset.
-              foregroundImage: AssetImage('assets/images/flutter_logo.png'),
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            const SizedBox(height: 10),
+            Text(
+              isConnected ? 'Connected' : 'Disconnected',
+              style: TextStyle(
+                color: isConnected ? Colors.green : Colors.red,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-            onTap: () {
-              // Navigate to the details page. If the user leaves and returns to
-              // the app after it has been killed while running in the
-              // background, the navigation stack is restored.
-              Navigator.restorablePushNamed(
-                context,
-                SampleItemDetailsView.routeName,
-              );
-            }
-          );
-        },
+            const SizedBox(height: 10),
+            TextButton(
+              onPressed: () async{
+                socket.connect();
+              },
+              child: const Text('CONNECT'),
+            ),
+            const SizedBox(height: 10),
+            TextButton(
+              onPressed: () async{
+                socket.emit('home', 'Hello from Flutter');
+              },
+              child: const Text('Send Message'),
+            ),
+            const SizedBox(height: 10),
+            TextButton(
+              onPressed: () async{
+                socket.disconnect();
+              },
+              child: const Text('DISCONNECT'),
+            ),
+          ],
+        ),
       ),
     );
   }
